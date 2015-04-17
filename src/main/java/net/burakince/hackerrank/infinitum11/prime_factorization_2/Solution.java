3684723bf7669b057dfb5113aa2b2a6087272ce5
Burak ince <burak.ince@linux.org.tr>
2015-04-18 01:54:35 +0300

    Prime Factorization performans problems fixed.

--- src/main/java/net/burakince/hackerrank/infinitum11/prime_factorization_2/Solution.java
+++ src/main/java/net/burakince/hackerrank/infinitum11/prime_factorization_2/Solution.java
@@ -1,7 +1,8 @@
 package net.burakince.hackerrank.infinitum11.prime_factorization_2;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,54 +12,36 @@ public class Solution {
 		Scanner in = new Scanner(System.in);
 		int result = 0;
 		List<Integer> results = null;
-		List<Integer> primes = new LinkedList<Integer>();
-		primes.add(2);
-		primes.add(3);
-		primes.add(5);
 
 		int testCount = in.nextInt();
-		int lastPrime = 5;
-		
-		for (int test = 0; test < testCount; test++) {
-			long number = in.nextInt();
+		List<Integer> numbers = new ArrayList<Integer>();
+
+		for (int i = 0; i < testCount; i++)
+			numbers.add(in.nextInt());
+
+		int maxNumber = Collections.max(numbers);
+
+		boolean[] primes = new boolean[maxNumber + 1];
+
+		Arrays.fill(primes, true);
+		primes[0] = primes[1] = false;
+		for (int i = 2; i < primes.length; i++) {
+			if (primes[i]) {
+				for (int j = 2; i * j < primes.length; j++) {
+					primes[i * j] = false;
+				}
+			}
+		}
+
+		for (Integer number : numbers) {
 			results = new ArrayList<Integer>();
 
-			while (number != 1) {
-				boolean primeFound = false;
-				
-				for (int prime : primes) {
+			for (int prime = 2; number != 1; prime++) {
+				if (primes[prime]) {
 					if (number % prime == 0) {
 						number /= prime;
 						results.add(prime);
-						primeFound = true;
-						break;
-					}
-				}
-				
-				if (primeFound)
-					continue;
-
-				for (int newPrime = lastPrime + 1; newPrime <= number; newPrime++) {
-					boolean primeMultiplesFound = false;
-					
-					for (int prime : primes) {
-						if (newPrime % prime == 0) {
-							primeMultiplesFound = true;
-							break;
-						}
-					}
-					
-					if (primeMultiplesFound) {
-						continue;
-					} else {
-						primes.add(newPrime);
-						lastPrime = newPrime;
-
-						if (number % newPrime == 0) {
-							number /= newPrime;
-							results.add(newPrime);
-							break;
-						}
+						prime--;
 					}
 				}
 			}
