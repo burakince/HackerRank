15d8f9e074ff4985194c362d58686214cac60812
Burak İnce <burak.ince@linux.org.tr>
2015-11-22 01:06:42 +0200

    FIXED: Running Time of Quicksort solution problems fixed.

--- src/main/java/net/burakince/hackerrank/quicksort4/Solution.java
+++ src/main/java/net/burakince/hackerrank/quicksort4/Solution.java
@@ -2,96 +2,79 @@ package net.burakince.hackerrank.quicksort4;
 
 import java.util.Scanner;
 
-class SortSwapCount {
-	
-	private int count = 0;
-
-	public void countUp() {
-		count++;
-	}
-
-	public int getCount() {
-		return count;
-	}
-
-}
-
 public class Solution {
 
 	public static void main(String[] args) {
 		final Scanner sc = new Scanner(System.in);
-		final int length = sc.nextInt();
+		int length = sc.nextInt();
 		final int[] numbers = new int[length];
 		for (int i = 0; i < length; i++) {
 			numbers[i] = sc.nextInt();
 		}
 		sc.close();
 
-		SortSwapCount quickSortSwapCount = new SortSwapCount();
-		SortSwapCount insertionSortSwapCount = new SortSwapCount();
+		int insertionShifts = insertionSort(copyArray(numbers));
+		int quickSortShifts = quickSort(numbers);
 
-		quickSort(numbers, quickSortSwapCount);
-		insertionSort(numbers, insertionSortSwapCount);
-
-		System.out.println(insertionSortSwapCount.getCount() - quickSortSwapCount.getCount());
+		System.out.println(insertionShifts - quickSortShifts);
 	}
 
-	private static void insertionSort(int[] numbers, SortSwapCount sortSwapCount) {
-		for (int i = 1; i < numbers.length; i++) {
-			int temp = numbers[i];
-			int j;
-			for (j = i - 1; j >= 0 && temp < numbers[j]; j--) {
-				sortSwapCount.countUp();
-				numbers[j + 1] = numbers[j];
-				numbers[j] = temp;
-			}
+	private static int[] copyArray(int[] numbers) {
+		final int length = numbers.length;
+		int[] newArray = new int[length];
+		for (int i = 0; i < length; i++) {
+			newArray[i] = numbers[i];
 		}
-	}
-
-	private static int[] quickSort(int[] numbers, SortSwapCount sortSwapCount) {
-		if (numbers.length < 2) {
-			return numbers;
-		} else if (numbers.length == 2) {
-			if (numbers[0] > numbers[1]) {
-				sortSwapCount.countUp();
-				int temp = numbers[0];
-				numbers[0] = numbers[1];
-				numbers[1] = temp;
-			}
-			return numbers;
-		}
-
-		int pivot = numbers[numbers.length - 1];
-		int[] left = new int[0];
-		int[] right = new int[0];
-		for (int i = 0; i < numbers.length - 1; i++) {
-			if (numbers[i] < pivot) {
-				sortSwapCount.countUp();
-				left = add(left, numbers[i]);
-			} else {
-				right = add(right, numbers[i]);
-			}
-		}
-		left = quickSort(left, sortSwapCount);
-		right = quickSort(right, sortSwapCount);
-
-		int[] result = new int[numbers.length];
-		System.arraycopy(left, 0, result, 0, left.length);
-		sortSwapCount.countUp();
-		result[left.length] = pivot;
-		System.arraycopy(right, 0, result, left.length + 1, right.length);
-		return result;
-	}
-
-	private static int[] add(int[] array, int number) {
-		int[] newArray = new int[array.length + 1];
-		if (array.length == 0) {
-			newArray[0] = number;
-			return newArray;
-		}
-		System.arraycopy(array, 0, newArray, 0, array.length);
-		newArray[newArray.length - 1] = number;
 		return newArray;
 	}
 
+	private static int insertionSort(int[] numbers) {
+		int shifts = 0;
+		final int length = numbers.length;
+		for (int i = 1; i < length; i++) {
+			int j = i;
+			int val = numbers[i];
+			while (j > 0 && numbers[j - 1] > val) {
+				numbers[j] = numbers[j - 1];
+				j--;
+				shifts++;
+			}
+			numbers[j] = val;
+		}
+		return shifts;
+	}
+
+	private static int quickSort(int[] numbers) {
+		return quickSort(numbers, 0, numbers.length);
+	}
+
+	private static int quickSort(int[] numbers, int min, int max) {
+		final int length = max - min;
+		if (length < 2) {
+			return 0;
+		}
+		int count = partition(numbers, min, max);
+		final int mid = min + count - 1;
+		count += quickSort(numbers, min, mid);
+		count += quickSort(numbers, mid + 1, max);
+		return count;
+	}
+
+	private static int partition(int[] numbers, int min, int max) {
+		final int pivot = numbers[--max];
+
+		int storeMax = min;
+		for (int i = min; i < max; i++) {
+			if (numbers[i] < pivot) {
+				final int temp = numbers[i];
+				numbers[i] = numbers[storeMax];
+				numbers[storeMax++] = temp;
+			}
+		}
+
+		numbers[max] = numbers[storeMax];
+		numbers[storeMax] = pivot;
+
+		return storeMax + 1 - min;
+	}
 }
