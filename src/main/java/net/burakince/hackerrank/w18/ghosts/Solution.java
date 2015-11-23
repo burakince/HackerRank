36429a526b4530ce4a79df8071bf50f095d0a1bd
Burak İnce <burak.ince@linux.org.tr>
2015-11-23 08:27:57 +0200

    FIXED: Ghosts solution error fixed.

--- src/main/java/net/burakince/hackerrank/w18/ghosts/Solution.java
+++ src/main/java/net/burakince/hackerrank/w18/ghosts/Solution.java
@@ -1,19 +1,17 @@
 package net.burakince.hackerrank.w18.ghosts;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Solution {
 
 	public static void main(String[] args) {
 		final Scanner sc = new Scanner(System.in);
-		final String[] line = sc.nextLine().split(" ");
+		final int towns = sc.nextInt();
+		final int streets = sc.nextInt();
+		final int houses = sc.nextInt();
+		final int aparts = sc.nextInt();
 		sc.close();
 
-		final int towns = Integer.parseInt(line[0]);
-		final int streets = Integer.parseInt(line[1]);
-		final int houses = Integer.parseInt(line[2]);
-		final int aparts = Integer.parseInt(line[3]);
 
 		int ghosts = 0;
 		for (int t = 1; t <= towns; t++) {
@@ -33,12 +31,10 @@ public class Solution {
 	public static boolean isGhostLives(int towns, int streets, int houses, int aparts) {
 		return Math.abs(towns - streets) % 3 == 0
 				&& (streets + houses) % 5 == 0
-				&& (towns % 4 == 0 || houses % 4 == 0)
-				&& BigInteger.valueOf(towns).gcd(BigInteger.valueOf(aparts)).intValue() == 1;
-//				&& GCD(towns, aparts) == 1;
+				&& (towns * houses) % 4 == 0
+				&& GCD(towns, aparts) == 1;
 	}
 
-	@SuppressWarnings("unused")
 	private static int GCD(int a, int b) {
 		if (b == 0) {
 			return Math.abs(a);
