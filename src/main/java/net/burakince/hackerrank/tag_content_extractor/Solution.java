c314646813dd39544fa83348292b738c202a7b1a
Burak İnce <burak.ince@linux.org.tr>
2015-12-10 23:57:38 +0200

    FIXED: Tag content extractor bug fixed.

--- src/main/java/net/burakince/hackerrank/tag_content_extractor/Solution.java
+++ src/main/java/net/burakince/hackerrank/tag_content_extractor/Solution.java
@@ -1,32 +1,41 @@
 package net.burakince.hackerrank.tag_content_extractor;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Solution {
 
-	private static Pattern pattern = Pattern.compile("<(\\w+\\s*\\w+)>(.+)<(\\/\\1)>");
-
 	public static void main(String[] args) {
 		final Scanner sc = new Scanner(System.in);
-		int tests = Integer.parseInt(sc.nextLine());
-		while (0 < tests--) {
+		int testCases = Integer.parseInt(sc.nextLine());
+		while (0 < testCases--) {
 			String line = sc.nextLine();
-			Matcher matcher = pattern.matcher(line);
-			if (matcher.find()) {
-				String found = matcher.group(2);
-				if (found.startsWith("<")) {
-					Matcher newMatcher = pattern.matcher(found);
-					if (newMatcher.find()) {
-						System.out.println(newMatcher.group(2));
-					}
-				} else if (found.contains("</")) {
-					System.out.println("None");
-				} else {
-					System.out.println(matcher.group(2));
+			int cur = 0;
+			boolean none = true;
+			for (;;) {
+				int start = line.indexOf("<", cur);
+				if (start < 0) {
+					break;
 				}
-			} else {
+				int end = line.indexOf(">", start);
+				if (end < 0) {
+					break;
+				}
+				String tag = line.substring(start + 1, end);
+				if (tag.length() == 0 || tag.charAt(0) == '/') {
+					cur = end + 1;
+					continue;
+				}
+				int bk = line.indexOf("</" + tag + ">");
+				if (bk >= 0) {
+					String candidate = line.substring(end + 1, bk);
+					if (candidate.length() > 0 && candidate.indexOf("<") < 0) {
+						none = false;
+						System.out.println(candidate);
+					}
+				}
+				cur = end + 1;
+			}
+			if (none) {
 				System.out.println("None");
 			}
 		}
